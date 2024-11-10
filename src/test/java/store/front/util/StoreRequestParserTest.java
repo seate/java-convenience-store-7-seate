bf4f81081b4b1d7ca02753d2424eae5e4ca65e99
siwoo <k74415834@gmail.com>
2024-11-11 05:58:31 +0900

    test(StoreRequestParserTest): 파서 테스트 추가

---
+++ src/test/java/store/front/util/StoreRequestParserTest.java
@@ -0,0 +1,36 @@
+package store.front.util;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import store.back.store.order.domain.ProductNameQuantity;
+import store.global.exception.CustomIllegalArgumentException;
+
+class StoreRequestParserTest {
+
+    @Test
+    void 정상_파싱() {
+        // given
+        String input = "[콜라-3],[에너지바-5]";
+
+        // when
+        List<ProductNameQuantity> productNameQuantities = StoreRequestParser.parseOrder(input);// 올바른 형식
+
+        // then
+        ProductNameQuantity first = productNameQuantities.getFirst();
+        Assertions.assertThat(first.productName()).isEqualTo("콜라");
+        Assertions.assertThat(first.quantity()).isEqualTo(3);
+        ProductNameQuantity second = productNameQuantities.get(1);
+        Assertions.assertThat(second.productName()).isEqualTo("에너지바");
+        Assertions.assertThat(second.quantity()).isEqualTo(5);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"[콜라-3", "[에너지바5]"})
+    void 파싱_예외(String input) {
+        Assertions.assertThatThrownBy(() -> StoreRequestParser.parseOrder(input))
+                .isInstanceOf(CustomIllegalArgumentException.class);
+    }
+}
