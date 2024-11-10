ae59765d382952bd0bcf0da9ac2e3b205ee75bfb
siwoo <k74415834@gmail.com>
2024-11-11 05:54:03 +0900

    feat(StoreRequestView): 사용자가 적게 가져온 수량 추가 입력 구현

--- src/main/java/store/front/view/StoreRequestView.java
+++ src/main/java/store/front/view/StoreRequestView.java
@@ -5,6 +5,7 @@ import java.util.List;
 import store.back.store.order.domain.ProductNameQuantity;
 import store.front.util.StoreRequestParser;
 import store.global.dto.request.OrderRequestDTO;
+import store.global.status.Agreement;
 
 public class StoreRequestView {
 
@@ -14,5 +15,13 @@ public class StoreRequestView {
         System.out.println();
 
         return OrderRequestDTO.createDefault(productNameQuantities);
+    }
+
+    public static Agreement isPromotionProductAddable(final String productName, final Integer quantity) {
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", productName, quantity);
+        Agreement agreement = StoreRequestParser.parseAgreement(Console.readLine());
+        System.out.println();
+
+        return agreement;
     }
 }
